872f31d9fc04f1965263c661555b26b91301d61a
antje12 <kaminskiantje07@gmail.com>
2025-07-11 21:30:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
-self.addEventListener('install', (e) => {
-  console.log('[ServiceWorker] Installiert');
-  e.waitUntil(
-    caches.open('maedchen-cache-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/icon-512.png'
-      ]);
-    })
-  );
+const CACHE_NAME = "arbeitszeit-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png",
+  "ping.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
